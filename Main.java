5dd248df7eac73a137b12dc91b127c3d8325606c
minki0807 <myong1306@gmail.com>
2025-01-09 23:06:10 +0900

    0109

--- Main.java
+++ Main.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("%7d%n",  11111);
-        System.out.printf("%-7d%n",  11111);
-        System.out.printf("%07d%n",  11111);
-        System.out.printf("%,7d%n",  11111);
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("정수를 입력하세요. --> ");
+        int intValue = scan.nextInt();
+
+        System.out.print("실수를 입력하세요. --> ");
+        double doubleValue = scan.nextDouble();
+
+        int sum = intValue + (int)doubleValue;
+        double sub = (double)intValue - doubleValue;
+        double mul = intValue * doubleValue;
+        int div = intValue / (int)doubleValue;
+
+        System.out.println("덧셈 결과(정수): " + sum);
+        System.out.println("뺄셈 결과(실수): " + sub);
+        System.out.println("곱셈 결과(실수): " + mul);
+        System.out.println("나눗셈 결과(정수): " + div);
     }
 }
