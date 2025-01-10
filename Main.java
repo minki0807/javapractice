1449c9f017782cd796e4e549a1aa7936229055ec
minki0807 <myong1306@gmail.com>
2025-01-10 12:17:58 +0900

    0110

--- Main.java
+++ Main.java
@@ -1,24 +1,14 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-
-        Scanner scan = new Scanner(System.in);
-
-        System.out.print("정수를 입력하세요. --> ");
-        int intValue = scan.nextInt();
-
-        System.out.print("실수를 입력하세요. --> ");
-        double doubleValue = scan.nextDouble();
-
-        int sum = intValue + (int)doubleValue;
-        double sub = (double)intValue - doubleValue;
-        double mul = intValue * doubleValue;
-        int div = intValue / (int)doubleValue;
-
-        System.out.println("덧셈 결과(정수): " + sum);
-        System.out.println("뺄셈 결과(실수): " + sub);
-        System.out.println("곱셈 결과(실수): " + mul);
-        System.out.println("나눗셈 결과(정수): " + div);
+        int score = 80;
+        if (score >= 90) {
+            System.out.println("A학점입니다.");
+        } else if (score >=80) {
+            System.out.println("B학점입니다.");
+        } else if (score >= 70) {
+            System.out.println("C학점입니다.");
+        } else {
+            System.out.println("D학점입니다.");
+        }
     }
 }
