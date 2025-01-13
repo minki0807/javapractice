492c21b417ceaa72c4083ecb8bab9f5dfc27a62a
minki0807 <myong1306@gmail.com>
2025-01-13 14:25:23 +0900

    0113

--- Main.java
+++ Main.java
@@ -1,14 +1,26 @@
+import  java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int score = 80;
-        if (score >= 90) {
-            System.out.println("A학점입니다.");
-        } else if (score >=80) {
-            System.out.println("B학점입니다.");
-        } else if (score >= 70) {
-            System.out.println("C학점입니다.");
-        } else {
-            System.out.println("D학점입니다.");
+        System.out.println("알파벳 소문자를 입력하세요 : ");
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        switch (str) {
+            case "a":
+                System.out.println("A");
+                break;
+
+            case "b":
+                System.out.println("B");
+                break;
+
+            case "c":
+                System.out.println("C");
+                break;
+
+            default:
+                System.out.println("일치하는 알파벳이 없습니다.");
         }
+        scan.close();
     }
 }
