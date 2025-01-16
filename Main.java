7a264ef1e43faf4e2035805b408c7aaf21575e16
minki0807 <myong1306@gmail.com>
2025-01-16 13:39:21 +0900

    0116-add 셀프체크

--- Main.java
+++ Main.java
@@ -1,19 +1,21 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int sum = 0;
-        while(true) {
-            System.out.println("숫자를 무한 반복해서 더하는 중입니다. ");
-            System.out.print("숫자를 입력하세요/ -> ");
-            int input = scanner.nextInt();
-            sum = sum + input;
-            System.out.println("결과: " + sum);
-            if(sum > 10000) {
-                break;
+        int[][] scoreArray = {
+                {85, 70, 90, 95},
+                {80, 95, 90, 75},
+                {75, 85, 90, 80}
+        };
+
+        System.out.println("학생들의 성적은 다음과 같습니다.");
+        for (int i = 0; i < scoreArray.length; i++) {
+            int sum = 0;
+            System.out.print("학생 " + (i + 1) + ": ");
+            for ( int j = 0; j < scoreArray[i].length; j++) {
+                System.out.print(scoreArray[i][j] + " ");
+                sum += scoreArray[i][j];
             }
+            double average = (double) sum / scoreArray[i].length;
+            System.out.println("| 평균: " + average);
         }
-        System.out.println("반복문을 탈출했습니다.");
     }
 }
