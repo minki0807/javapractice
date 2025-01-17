298d2047c3fae3bde43349e18eba8b57a9eb88f0
minki0807 <myong1306@gmail.com>
2025-01-17 14:27:22 +0900

    0117 - 218p 1분퀴즈

---
+++ src/ch07/Main.java
@@ -0,0 +1,8 @@
+package src.ch07;
+
+public class Main {
+    public static void main(String[] args) {
+        Car otherCar = new Car("Genesis", 2025, "Red");
+        otherCar.carInfo();
+    }
+}
