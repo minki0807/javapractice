6b95c0ef37817b18b0ce54f4d9c56e354a21a2c8
minki0807 <myong1306@gmail.com>
2025-01-23 10:08:40 +0900

    0123 - add 8.4 인터페이스 실습

---
+++ src/ch08_interface/BankAccount.java
@@ -0,0 +1,7 @@
+package src.ch08_interface;
+
+public interface BankAccount {
+    void withdraw(double amount);
+    void deposit(double amount);
+    void displayAccountInfo();
+}
---
+++ src/ch08_interface/InterestBearing.java
@@ -0,0 +1,7 @@
+package src.ch08_interface;
+
+// 이자율을 적용하는 인터페이스
+public interface InterestBearing {
+    void addInterest(); // 추상 메서드: 이자를 추가하는 기능
+}
+
