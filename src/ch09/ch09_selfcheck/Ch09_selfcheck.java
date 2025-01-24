ab60b59e50dfda78f401a66cbb8ea448781dd950
minki0807 <myong1306@gmail.com>
2025-01-24 11:03:06 +0900

    0124 - refactor

--- src/ch09/ch09_selfcheck/Ch09_selfcheck.java
+++ src/ch09/ch09_selfcheck/Ch09_selfcheck.java
@@ -1,2 +1,15 @@
-package src.ch09.ch09_2;public class Ch09_selfcheck {
+package src.ch09.ch09_selfcheck;
+
+public class Ch09_selfcheck {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(100000);
+        try {
+            account.deposit(50000);
+            account.withdraw(200000);
+        } catch (LowBalanceException e) {
+            System.out.println("오류: 잔액이 부족합니다.");
+        } finally {
+            System.out.println("현재 잔액: " + account.getBalance() + "원");
+        }
+    }
 }
