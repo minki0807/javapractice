721a2aba7d8fdf285c82c7db585ce37e73db97b8
minki0807 <myong1306@gmail.com>
2025-01-30 18:35:37 +0900

    0130 - add Ch11_4 실습2

--- src/ch11/Ch11_4.java
+++ src/ch11/Ch11_4.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 
 public class Ch11_4 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("이름을 입력하세요. --> ");
-        String name = scanner.nextLine();
-        System.out.println("안녕, " + name + "!");
+        System.out.println("Hello, Java!");
+        int a = 10;
+        int b = 20;
+        System.out.printf("%d과 %d의 합: %d%n", a, b, a+b);
     }
 }
