173d4f488e378febd69982605a7a75394faa3e5d
minki0807 <myong1306@gmail.com>
2025-01-30 18:38:30 +0900

    0130 - add Ch11_4 실습3

--- src/ch11/Ch11_4.java
+++ src/ch11/Ch11_4.java
@@ -6,8 +6,12 @@ import java.util.Scanner;
 public class Ch11_4 {
     public static void main(String[] args) {
         System.out.println("Hello, Java!");
-        int a = 10;
-        int b = 20;
-        System.out.printf("%d과 %d의 합: %d%n", a, b, a+b);
+        try {
+            int result = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.err.println("Error: Division by zero!");
+        }
+
+        System.out.println("프로그램 실행 중");
     }
 }
